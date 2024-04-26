f546ccb32f201f11e67a51bfef078ea401d90f05
wingsvn <wing-san.lee@live.nl>
2024-04-26 10:00:15 +0200

    v1 no refresh enhancement  met js

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 
-
+// carrousel
 // Get the stories content list
 const storiesContent = document.querySelector('.stories-content');
 
@@ -42,3 +42,66 @@ nextButton.addEventListener('click', () => {
         behavior: 'smooth' // Smooth scrolling effect
     });
 });
+
+// enhanced user experience: no refresh 
+// Selecteer alle bestelformulieren
+let forms = document.querySelectorAll('form.like-or-unlike-playlist')
+
+// Loop door al die formulieren
+forms.forEach(function(form) {
+
+    // Luister naar het submit event
+    form.addEventListener('submit', function(event) {
+        //console.log(form)
+        //console.log(this.action)
+        // Het this object refereert hier naar het formulier zelf
+
+		// Lees de data van het formulier in
+		// https://developer.mozilla.org/en-US/docs/Web/API/FormData
+        let data = new FormData(this)
+
+        // Voeg een extra eigenschap aan de formulierdata toe
+		// Deze gaan we server-side gebruiken om iets anders terug te sturen
+		data.append('enhanced', true)
+
+        // Waarschijnlijk wil je op deze plek ook een loading state
+		// maken, maar daar gaan we volgende week mee aan de slag
+
+		// Gebruik een client-side fetch om een POST te doen naar de server
+		// Als URL gebruiken we this.action
+		// https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
+        fetch(this.action, {
+
+            // Als method gebruiken we this.method (waarschijnlijk POST)
+            method: this.method,
+
+            // Als body geven de data van het formulier mee (inclusief de extra eigenschap)
+			// https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams
+            body: new URLSearchParams(data)
+
+        }).then(function(response) {
+            // Als de server een antwoord geeft, krijgen we een stream terug
+			// We willen hiervan de text gebruiken, wat in dit geval HTML teruggeeft
+            return response.text();
+
+        }).then(function(responseHTML) {
+            //console.log(responseHTML)
+            // En de HTML kunnen we gebruiken om onze DOM aan te passen
+            document.querySelector('.suggested-playlist-container').innerHTML = responseHTML;
+            console.log(responseHTML)
+
+            // En hier kun je bijvoorbeeld nog wat extra's doen om duidelijker te maken
+			// dat er iets gebeurd is op de pagina
+			// document.querySelector('').open = true;
+
+		    // Een eventuele loading state haal je hier ook weer weg
+        });
+
+    // Als alles gelukt is, voorkom dan de submit van de browser zelf
+	// Stel dat je hierboven een tikfout hebt gemaakt, of de browser ondersteunt
+	// een bepaalde feature hierboven niet (bijvoorbeeld FormData), dan krijg je
+	// een error en wordt de volgende regel nooit uitgevoerd. De browser valt dan
+	// automatisch terug naar de standaard POST, wat prima is.
+	    event.preventDefault()
+    })
+})
--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-console.log('Hier komt je server voor Sprint 10. Gebruik uit Sprint 9 alleen de code die je mee wilt nemen.')
-
-
-// opzetten van de webserver 
+// opzetten van de webserver
 import express from 'express'
 import fetchJson from './helpers/fetch-json.js'
+
+const apiUrl = "https://fdnd-agency.directus.app/items";
 
 const app = express()
 app.set('view engine', 'ejs')
@@ -13,148 +12,124 @@ app.use(express.urlencoded({extended: true}))
 
 let favorites = {}
 
-// routes aanmaken
-
+// ROUTES
 // maak een GET route voor de index (home)
 app.get('/', function(request, response) {
-    Promise.all([ // Fetch data from all endpoints concurrently using Promise.all()
-    fetchJson('https://fdnd-agency.directus.app/items/tm_story'),
-    fetchJson('https://fdnd-agency.directus.app/items/tm_language'),
-    fetchJson('https://fdnd-agency.directus.app/items/tm_audio'),
-    fetchJson('https://fdnd-agency.directus.app/items/tm_playlist')
-  ]).then(([storyData, languageData, audioData, playlistData]) => {
+  Promise.all([ // Fetch data from all endpoints concurrently using Promise.all()
+    fetchJson(apiUrl + "/tm_story"),
+    fetchJson(apiUrl + "/tm_language"),
+    fetchJson(apiUrl + "/tm_playlist"),
+    fetchJson(apiUrl + "/tm_audio"),
+  ]).then(([storyData, languageData, playlistData, audioData]) => {
 
-    response.render('home', {
-        stories: storyData.data, 
-        languages: languageData.data,
-        playlists: playlistData.data,
-        audio: audioData.data}) 
-    })
+  response.render('home', {
+      stories: storyData.data, 
+      languages: languageData.data,
+      playlists: playlistData.data,
+      audio: audioData.data
+    }) 
   })
+})
 
-// maak een GET route voor lessons 
+// maak een GET route voor testing
+app.get("/testing", function (request, response) {
+  response.render("testing");
+});
+
+// maak een GET route voor lessons
 app.get('/lessons', function(request, response) {
-    Promise.all([ // Fetch data from all endpoints concurrently using Promise.all()
-    fetchJson('https://fdnd-agency.directus.app/items/tm_story'),
-    fetchJson('https://fdnd-agency.directus.app/items/tm_language'),
-    fetchJson('https://fdnd-agency.directus.app/items/tm_audio'),
-    fetchJson('https://fdnd-agency.directus.app/items/tm_playlist')
+  Promise.all([ // Fetch data from all endpoints concurrently using Promise.all()
+    fetchJson(apiUrl + "/tm_story"),
+    fetchJson(apiUrl + "/tm_language"),
+    fetchJson(apiUrl + "/tm_audio"),
+    fetchJson(apiUrl + "/tm_playlist"),
   ]).then(([storyData, languageData, audioData, playlistData]) => {
 
-    response.render('lessons', {
-        stories: storyData.data, 
-        languages: languageData.data, 
-        playlists: playlistData.data, 
-        audio: audioData.data,
-        favorites: favorites,
-        likedPlaylist: request.query
-      }) 
-    })
+  response.render('lessons', {
+      stories: storyData.data, 
+      languages: languageData.data,
+      playlists: playlistData.data,
+      audio: audioData.data,
+      favorites: favorites,
+      likedPlaylist: request.query
+    }) 
   })
+})
 
 // maak een POST route voor lessons (like)
 app.post('/:playlistId/like-or-unlike', function(request, response) {
   const playlistId = Number(request.params.playlistId);
-  const action = request.body.action; // Retrieve the value of the 'actie' parameter from the form
+  const actie = request.body.actie; // Retrieve the value of the 'actie' parameter from the form
 
   // Implement the logic to handle liking or unliking the playlist
-  if (action === 'like') {
+  if (actie === 'like') {
     favorites[playlistId] = true
     // Handle 'like' action
-  } else if (action === 'unlike') {
+  } else if (actie === 'unlike') {
     favorites[playlistId] = false
-
-    // Handle 'unlike' action
   }
 
-  response.redirect('/lessons?liked-playlist='+playlistId+'#suggested-playlist')
+  // Als onze POST request de 'enhanced' property heeft, werd deze door
+  // client-side JS afgevuurd. In dat geval willen we alleen een partial
+  // renderen/terugsturen. De client-side JS gebruikt dat stukje HTML om
+  // de UI state snel en makkelijk te updaten.
+  // Als er geen 'enhanced' property is, dan was het een 'normale' browser
+  // POST, en sturen we de gebruiker door naar het overzicht van het mandje.
+  // if (request.body.enhanced) {
+  //   response.render('partials/suggested-playlist', {favorites: favorites})
+  // } else {
+  //   response.redirect(303, '/lessons')
+  // }
+
+  // Handle 'unlike' action
+   response.redirect('/lessons?liked-playlist='+playlistId)
   })
 
 // maak een GET route voor stories
-app.get('/lessons/stories', function(request, response) {
-    Promise.all([ // Fetch data from all endpoints concurrently using Promise.all()
-    fetchJson('https://fdnd-agency.directus.app/items/tm_story'),
-    fetchJson('https://fdnd-agency.directus.app/items/tm_language'),
-    fetchJson('https://fdnd-agency.directus.app/items/tm_audio'),
-    fetchJson('https://fdnd-agency.directus.app/items/tm_playlist')
-  ]).then(([storyData, languageData, audioData, playlistData]) => {
-
-    response.render('stories', {
-        stories: storyData.data, 
-        languages: languageData.data, 
-        playlists: playlistData.data, 
-        audio: audioData.data}) 
-    })
+app.get('/stories', function(request, response) {
+  Promise.all([ // Fetch data from all endpoints concurrently using Promise.all()
+    fetchJson(apiUrl + "/tm_story"),
+    fetchJson(apiUrl + "/tm_language"),
+    fetchJson(apiUrl + "/tm_playlist"),
+    fetchJson(apiUrl + "/tm_audio"),
+ ]).then(([storyData, languageData, audioData, playlistData]) => {
+  
+  response.render('stories', {
+      stories: storyData.data, 
+      languages: languageData.data,
+      playlists: playlistData.data,
+      audio: audioData.data     
+    }) 
   })
+})
 
 // maak een GET route voor playlist
-app.get('lessons/playlist', function(request, response) {
-    Promise.all([ // Fetch data from all endpoints concurrently using Promise.all()
-    fetchJson('https://fdnd-agency.directus.app/items/tm_story'),
-    fetchJson('https://fdnd-agency.directus.app/items/tm_language'),
-    fetchJson('https://fdnd-agency.directus.app/items/tm_audio'),
-    fetchJson('https://fdnd-agency.directus.app/items/tm_playlist')
+app.get('/playlist', function(request, response) {
+  Promise.all([ // Fetch data from all endpoints concurrently using Promise.all()
+    fetchJson(apiUrl + "/tm_story"),
+    fetchJson(apiUrl + "/tm_language"),
+    fetchJson(apiUrl + "/tm_playlist"),
+    fetchJson(apiUrl + "/tm_audio"),
   ]).then(([storyData, languageData, audioData, playlistData]) => {
-
+  
     response.render('playlist', {
-        stories: storyData.data, 
-        languages: languageData.data, 
-        playlists: playlistData.data, 
-        audio: audioData.data}) 
-    })
+      stories: storyData.data, 
+        languages: languageData.data,
+        playlists: playlistData.data,
+        audio: audioData.data
+    }) 
   })
+})
 
-// maak een GET route voor testing 
-app.get('/testing', function(request, response) {
-    Promise.all([ // Fetch data from all endpoints concurrently using Promise.all()
-    fetchJson('https://fdnd-agency.directus.app/items/tm_story'),
-    fetchJson('https://fdnd-agency.directus.app/items/tm_language'),
-    fetchJson('https://fdnd-agency.directus.app/items/tm_audio'),
-    fetchJson('https://fdnd-agency.directus.app/items/tm_playlist')
-  ]).then(([storyData, languageData, audioData, playlistData]) => {
-
-    response.render('testing', {
-        stories: storyData.data, 
-        languages: languageData.data, 
-        playlists: playlistData.data, 
-        audio: audioData.data}) 
-    })
-  })
-
-// maak een GET route voor statistics
-app.get('/statistics', function(request, response) {
-    Promise.all([ // Fetch data from all endpoints concurrently using Promise.all()
-    fetchJson('https://fdnd-agency.directus.app/items/tm_story'),
-    fetchJson('https://fdnd-agency.directus.app/items/tm_language'),
-    fetchJson('https://fdnd-agency.directus.app/items/tm_audio'),
-    fetchJson('https://fdnd-agency.directus.app/items/tm_playlist')
-  ]).then(([storyData, languageData, audioData, playlistData]) => {
-
-    response.render('statistics', {
-        stories: storyData.data, 
-        languages: languageData.data, 
-        playlists: playlistData.data, 
-        audio: audioData.data}) 
-    })
-  })
+  // maak een GET route voor statistics
+  app.get("/statistics", function (request, response) {
+    response.render("statistics");
+  });
 
 // maak een GET route voor profile
-app.get('/profile', function(request, response) {
-    Promise.all([ // Fetch data from all endpoints concurrently using Promise.all()
-    fetchJson('https://fdnd-agency.directus.app/items/tm_story'),
-    fetchJson('https://fdnd-agency.directus.app/items/tm_language'),
-    fetchJson('https://fdnd-agency.directus.app/items/tm_audio'),
-    fetchJson('https://fdnd-agency.directus.app/items/tm_playlist')
-  ]).then(([storyData, languageData, audioData, playlistData]) => {
-
-    response.render('profile', {
-        stories: storyData.data, 
-        languages: languageData.data, 
-        playlists: playlistData.data, 
-        audio: audioData.data}) 
-    })
-  })
-
+  app.get("/profile", function (request, response) {
+    response.render("profile");
+  });
 
 // start de webserver
 app.set('port', process.env.PORT || 8000)
